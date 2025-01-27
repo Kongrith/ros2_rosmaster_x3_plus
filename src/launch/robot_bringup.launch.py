a65f86d78047870ee79568f4fd84ebc2daf9734c
kongrith <capuchino_cartoon@hotmail.com>
2025-01-27 20:16:51 +0000

    add ydlidar to bringup

--- src/launch/robot_bringup.launch.py
+++ src/launch/robot_bringup.launch.py
@@ -1,8 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription		# inlcude launch file ของตัวอื่นๆ
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 # launch description
 def generate_launch_description():
+
+	# เอาไว้ต่อ path
+	lidar_launch_path = PathJoinSubstitution([FindPackageShare('ydlidar_ros2_driver'),
+										   'launch', 'ydlidar_launch.py'])
+
 	return LaunchDescription([
 		# Node(
 		# 	package="micro_ros_agent",
@@ -18,6 +27,7 @@ def generate_launch_description():
 		# 	# arguments=['serial', '--dev', '/dev/ttyACM0'],
 		# 	output='screen',
 		# ),
+
 		Node(
 			package="mypkg",
 			executable="robot_run",
@@ -25,6 +35,11 @@ def generate_launch_description():
 			# arguments=['serial', '--dev', '/dev/ttyACM0'],
 			output='screen',
 		),
+
+		# เพิ่ม launch file ตัวอื่น
+		IncludeLaunchDescription(
+			PythonLaunchDescriptionSource(lidar_launch_path)
+		)
 	])
 
 
